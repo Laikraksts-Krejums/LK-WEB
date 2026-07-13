b7cf7237666ee600a25e1ab0766f8f4c7ac2c1c4
Nikolass Timaks <ntimaks27@gmail.com>
2026-07-13 15:06:31 +0300

    feat: add StudioIcon component and configure theme in Sanity

---
+++ src/sanity/components/StudioIcon.tsx
@@ -0,0 +1,9 @@
+export function StudioIcon() {
+  return (
+    <img
+      src="/icon.jpg"
+      alt=""
+      style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "20%" }}
+    />
+  );
+}
